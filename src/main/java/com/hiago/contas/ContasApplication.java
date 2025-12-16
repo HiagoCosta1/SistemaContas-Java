9b43fcce6baf868ae464305fe356393aad57f076
HiagoCosta1 <hiagocostazero@gmail.com>
2025-12-16 16:25:33 -0300

    Primeiro Commit

---
+++ src/main/java/com/hiago/contas/ContasApplication.java
@@ -0,0 +1,16 @@
+package com.hiago.contas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import domain.Cliente;
+
+@SpringBootApplication
+public class ContasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContasApplication.class, args);
+		
+	}
+
+}
---
+++ src/main/java/domain/Cliente.java
@@ -0,0 +1,87 @@
+package domain;
+
+import java.util.Objects;
+
+public class Cliente {
+	
+	private Long id;
+	private String nome;
+	private String email;
+	private String telefone;
+	
+	public Cliente() {
+		
+	}
+
+	public Cliente(String nome, String email, String telefone) {
+		super();
+		this.nome = nome;
+		this.email = email;
+		this.telefone = telefone;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	
+	
+	@Override
+	public String toString() {
+		return "Cliente {" +
+				"id = " + id + '\'' +
+				", nome = " + nome + '\'' +
+				", email = " + email + '\'' +
+				", telefone = " + telefone + '\'' +
+				"}";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cliente other = (Cliente) obj;
+		return Objects.equals(email, other.email) && Objects.equals(id, other.id) && Objects.equals(nome, other.nome)
+				&& Objects.equals(telefone, other.telefone);
+	}
+	
+	
+	
+
+}
